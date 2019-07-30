d3ea8500ec7b0f1079ffc40c98c5745d80c16941
hkantare <hkantare@in.ibm.com>
2019-07-30 14:52:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
-provider "ibm" {
-  bluemix_api_key    = "${var.ibm_bmx_api_key}"
-}
+
 data "ibm_container_cluster_config" "cluster_cfg" {
  cluster_name_id             = "${var.cluster}"
  config_dir                  = "/tmp"
